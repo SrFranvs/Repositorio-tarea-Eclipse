eb356e3803cfbe7fd144062132bd537bb76405ee
SrFranvs <1885453@alu.murciaeduca.es>
2024-02-14 00:59:21 +0100

    4.2 Añadir 2 archivos con extensión .java

---
+++ HolaDavid/src/AdiosDavid.java
@@ -0,0 +1,6 @@
+
+public class AdiosDavid {
+	public static void main(String[] args) {
+		System.out.print("Adios David");
+	}
+}
---
+++ HolaDavid/src/HeyHey.java
@@ -0,0 +1,6 @@
+
+public class HeyHey {
+	public static void main(String[] args) {
+		System.out.print("Hey Hey");
+	}
+}
